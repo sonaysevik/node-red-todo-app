bd7b6a977c8268d50b814df08de79a5d16976ffc
Sevik, Sonay <sonaysevik@gmail.com>
2019-11-12 16:54:29 +0000

    initial changes

---
+++ settings.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+
+module.exports = {
+    uiPort: process.env.PORT,
+    credentialSecret: process.env.NODE_RED_CREDENTIAL_SECRET,
+    httpAdminRoot: false,
+    httpStatic: path.join(__dirname,"public")
+}
+
